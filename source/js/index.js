93a3345bd4d2ccf3ef0ef09fdb13ae184227df6c
Daniil <eo18622@yandex.ru>
2022-11-16 15:06:35 +0300

    слайдер

--- source/js/index.js
+++ source/js/index.js
@@ -28,3 +28,24 @@ if (menuItemNode) {
         })
     }
 }
+
+
+// Инициализируем слайдер 
+new Swiper('.image-slider', {
+    // Стрелки
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next'
+    },
+    // Навигация
+    // Булеты, текущее положение
+    pagination: {
+        el: '.swiper-pagination',
+        // Буллеты
+        clickable: true,
+        // Динамические булеты
+        dynamicBullets: true,
+    },
+    // Отступ между слайдами
+    spaceBetween: 30,
+});
